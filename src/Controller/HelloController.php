d60a0227af163e62b4615ab4c7f775194f2224fe
hiroshi_yoshida <u.dance.gm@gmail.com>
2019-11-07 01:16:53 +0900

    section 2-2

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,19 +5,13 @@ use App\Controller\AppController;
 
 class HelloController extends AppController
 {
-        public $autoRender = false;
-        private $data = [
-            ['name' => 'taro', 'mail' => 'taro@yamada', 'tel' => '090-999-999'],
-            ['name' => 'hanako', 'mail' => 'hanako@flower', 'tel' => '080-888-888'],
-            ['name' => 'sachiko', 'mail' => 'sachiko@happy', 'tel' => '070-777-777']
-        ];
-
-        public function index()
-        {
-            $id = 0;
-            if (isset($this->request->query['id'])) {
-                $id = $this->request->query['id'];
-            }
-            echo json_encode($this->data[$id]);
-        }
+    public function index()
+    {
+          $this->viewBuilder()->enableAutoLayout(false);
+          $values = [
+                        'title' => 'Hello!',
+                        'message' => 'This is message!'
+                    ];
+                    $this->set($values);
+    }
 }
---
+++ src/Template/Hello/index.ctp
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?=$title ?></title>
+	<style>
+	h1 {font-size: 48pt;
+	margin: 0 0 10px 0; padding: 0 20px; color: white;
+	background: linear-gradient(to right, #aaa, #fff); }
+	p {font-size: 14pt; color: #666;}
+	</style>
+</head>
+<body>
+	<header class="row">
+		<h1><?=$title ?></h1>
+	</header>
+	<div class="row">
+		<p><?=$message ?></p>
+	</div>
+</body>
+</html>
